6a6e7c9c26d004f65884f0564316253e10ae4e96
panqingdao <panqingdao@rd.tuan800.com>
2016-01-21 23:43:40 +0800

    React生命周期测试

--- react/load.js
+++ react/load.js
@@ -31,7 +31,178 @@ var Loading = React.createClass({
   }
 });
 
+var OK = React.createClass({
+
+  getDefaultProps: function() {
+    console.log('OK----getDefaultProps');
+    return {
+      name: 'panqd'
+    };
+  },
+
+  getInitialState: function() {
+    console.log('OK----getInitialState');
+    return {
+      show: true
+    };
+  },
+
+  componentWillMount: function() {
+    console.log('OK----componentWillMount');
+  },
+
+  componentDidMount() {
+    console.log('OK----componentDidMount');
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    console.log('OK----shouldComponentUpdate');
+    return true;
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    console.log('OK----componentWillReceiveProps');
+  },
+
+  componentWillUpdate: function(nextProps, nextState) {
+    console.log('OK----componentWillUpdate');
+  },
+
+  componentDidUpdate: function(prevProps, prevState) {
+    console.log('OK----componentDidUpdate');
+  },
+
+  componentWillUnmount: function() {
+    console.log('OK----componentWillUnmount');
+  },
+
+  render: function() {
+    console.log('OK----render');
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+});
+
+var OK2 = React.createClass({
+
+  getDefaultProps: function() {
+    console.log('KingDomPan----getDefaultProps');
+    return {
+      name: 'panqd'
+    };
+  },
+
+  getInitialState: function() {
+    console.log('KingDomPan----getInitialState');
+    return {
+      show: true
+    };
+  },
+
+  componentWillMount: function() {
+    console.log('KingDomPan----componentWillMount');
+  },
+
+  componentDidMount() {
+    console.log('KingDomPan----componentDidMount');
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    console.log('KingDomPan----shouldComponentUpdate');
+    return true;
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    console.log('KingDomPan----componentWillReceiveProps');
+  },
+
+  componentWillUpdate: function(nextProps, nextState) {
+    console.log('KingDomPan----componentWillUpdate');
+  },
+
+  componentDidUpdate: function(prevProps, prevState) {
+    console.log('KingDomPan----componentDidUpdate');
+  },
+
+  componentWillUnmount: function() {
+    console.log('KingDomPan----componentWillUnmount');
+  },
+
+  render: function() {
+    console.log('KingDomPan----render');
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+});
+
+
+var Input = React.createClass({
+
+  getDefaultProps: function() {
+    console.log('getDefaultProps');
+    return {
+
+    };
+  },
+
+  getInitialState: function() {
+    console.log('getInitialState');
+    return {
+      show: true
+    };
+  },
+
+  componentWillMount: function() {
+    console.log('componentWillMount');
+  },
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    console.log('shouldComponentUpdate');
+    return true;
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    console.log('componentWillReceiveProps');
+  },
+
+  componentWillUpdate: function(nextProps, nextState) {
+    console.log('componentWillUpdate');
+  },
+
+  componentDidUpdate: function(prevProps, prevState) {
+    console.log('componentDidUpdate');
+  },
+
+  componentWillUnmount: function() {
+    console.log('componentWillUnmount');
+  },
+
+  _onClick() {
+    this.setState({
+      show: !this.state.show
+    });
+  },
+
+  render: function() {
+    console.log('render');
+    return (
+      <div>
+        <input style={{display: this.state.show ? '' : 'none'}} />
+          <OK name={this.state.show}/>
+          <OK2 />
+        <button onClick={this._onClick}>DDDD</button>
+      </div>
+    );
+  }
+});
+
 React.render(
-  <Loading />,
+  <Input />,
   document.getElementById('example')
 );
